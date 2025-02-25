d07102cd4b840316055e159b194ac4fdf2a18ab7
Karl Horky <karl.horky@gmail.com>
2025-02-25 16:33:48 +0100

    Add X-Notion-Space-Id header to resolve HTTP 504 errors (#312)

--- index.ts
+++ index.ts
@@ -54,176 +54,191 @@ type Task = {
   };
 };
 
-const client = axios.create({
-  // Notion unofficial API
-  baseURL: 'https://www.notion.so/api/v3',
-  headers: {
-    Cookie: `token_v2=${process.env.NOTION_TOKEN}`,
-  },
-});
+for (const [spaceId, spaceBlocks] of Object.entries(
+  Object.groupBy(blocks, (block) => block.spaceId),
+)) {
+  if (!spaceBlocks) continue;
 
-function delay(ms: number) {
   console.log(
-    `Waiting ${ms / 1000} second${ms > 1000 ? 's' : ''} before polling again...`,
+    `Exporting ${spaceBlocks.length} blocks from space ${spaceId}...`,
   );
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
 
-// Enqueue all export tasks immediately, without waiting for the
-// export tasks to complete
-const enqueuedBlocks = await pMap(blocks, async (block) => {
-  const {
-    data: { taskId },
-  }: { data: { taskId: string } } = await client.post('enqueueTask', {
-    task: {
-      eventName: 'exportBlock',
-      request: {
-        block: {
-          id: block.id,
-          spaceId: block.spaceId,
-        },
-        exportOptions: {
-          exportType: 'markdown',
-          locale: 'en',
-          timeZone: 'Europe/Vienna',
-        },
-        recursive: block.recursive,
-      },
+  const client = axios.create({
+    // Notion unofficial API
+    baseURL: 'https://www.notion.so/api/v3',
+    headers: {
+      Cookie: `token_v2=${process.env.NOTION_TOKEN}`,
+      'X-Notion-Space-Id': spaceId,
     },
   });
 
-  if (!taskId) {
-    throw new Error('No taskId returned from enqueueTask');
+  function delay(ms: number) {
+    console.log(
+      `Waiting ${ms / 1000} second${ms > 1000 ? 's' : ''} before polling again...`,
+    );
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
   }
 
-  console.log(`Started export of block ${block.dirName} as task ${taskId}`);
-
-  const task: BlockTask = {
-    id: taskId,
-    state: null,
-    status: {
-      pagesExported: null,
-      exportURL: null,
-    },
-  };
-
-  return {
-    ...block,
-    task: task,
-  };
-});
-
-let retries = 0;
-
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-while (true) {
-  const incompleteEnqueuedBlocks = enqueuedBlocks.filter(
-    ({ task }) => task.state !== 'success',
-  );
-
-  const taskIds = incompleteEnqueuedBlocks.map(({ task }) => task.id);
-
-  try {
+  // Enqueue all export tasks immediately, without waiting for the
+  // export tasks to complete
+  const enqueuedBlocks = await pMap(spaceBlocks, async (block) => {
     const {
-      data: { results },
-      headers: { 'set-cookie': getTasksRequestCookies },
-    }: { data: { results: Task[] }; headers: { 'set-cookie': string[] } } =
-      await client.post('getTasks', {
-        taskIds: taskIds,
-      });
-
-    const blocksWithTaskProgress = results.reduce(
-      (blocksAcc, task) => {
-        const block = enqueuedBlocks.find(({ task: { id } }) => id === task.id);
-
-        if (!block || !task.status) return blocksAcc;
-
-        // Mutate original object in enqueuedBlocks for while loop
-        // exit condition
-        block.task.state = task.state;
-        block.task.status.pagesExported = task.status.pagesExported;
-        block.task.status.exportURL = task.status.exportURL;
-
-        return blocksAcc.concat(block);
+      data: { taskId },
+    }: { data: { taskId: string } } = await client.post('enqueueTask', {
+      task: {
+        eventName: 'exportBlock',
+        request: {
+          block: {
+            id: block.id,
+            spaceId: block.spaceId,
+          },
+          exportOptions: {
+            exportType: 'markdown',
+            locale: 'en',
+            timeZone: 'Europe/Vienna',
+          },
+          recursive: block.recursive,
+        },
       },
-      [] as typeof incompleteEnqueuedBlocks,
+    });
+
+    if (!taskId) {
+      throw new Error('No taskId returned from enqueueTask');
+    }
+
+    console.log(`Started export of block ${block.dirName} as task ${taskId}`);
+
+    const task: BlockTask = {
+      id: taskId,
+      state: null,
+      status: {
+        pagesExported: null,
+        exportURL: null,
+      },
+    };
+
+    return {
+      ...block,
+      task: task,
+    };
+  });
+
+  let retries = 0;
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  while (true) {
+    const incompleteEnqueuedBlocks = enqueuedBlocks.filter(
+      ({ task }) => task.state !== 'success',
     );
 
-    for (const block of blocksWithTaskProgress) {
-      console.log(
-        `Exported ${block.task.status.pagesExported} pages for ${block.dirName}`,
+    const taskIds = incompleteEnqueuedBlocks.map(({ task }) => task.id);
+
+    try {
+      const {
+        data: { results },
+        headers: { 'set-cookie': getTasksRequestCookies },
+      }: { data: { results: Task[] }; headers: { 'set-cookie': string[] } } =
+        await client.post('getTasks', {
+          taskIds: taskIds,
+        });
+
+      const blocksWithTaskProgress = results.reduce(
+        (blocksAcc, task) => {
+          const block = enqueuedBlocks.find(
+            ({ task: { id } }) => id === task.id,
+          );
+
+          if (!block || !task.status) return blocksAcc;
+
+          // Mutate original object in enqueuedBlocks for while loop
+          // exit condition
+          block.task.state = task.state;
+          block.task.status.pagesExported = task.status.pagesExported;
+          block.task.status.exportURL = task.status.exportURL;
+
+          return blocksAcc.concat(block);
+        },
+        [] as typeof incompleteEnqueuedBlocks,
       );
 
-      if (block.task.state === 'success') {
-        const backupDirPath = path.join(
-          process.cwd(),
-          'exports',
-          block.dirName,
+      for (const block of blocksWithTaskProgress) {
+        console.log(
+          `Exported ${block.task.status.pagesExported} pages for ${block.dirName}`,
         );
 
-        const temporaryZipPath = path.join(
-          process.cwd(),
-          'exports',
-          `${block.dirName}.zip`,
-        );
+        if (block.task.state === 'success') {
+          const backupDirPath = path.join(
+            process.cwd(),
+            'exports',
+            block.dirName,
+          );
 
-        console.log(`Export finished for ${block.dirName}`);
+          const temporaryZipPath = path.join(
+            process.cwd(),
+            'exports',
+            `${block.dirName}.zip`,
+          );
 
-        const response = await client<Stream>({
-          method: 'GET',
-          url: block.task.status.exportURL || undefined,
-          responseType: 'stream',
-          headers: {
-            Cookie: getTasksRequestCookies.find((cookie) =>
-              cookie.includes('file_token='),
-            ),
-          },
-        });
+          console.log(`Export finished for ${block.dirName}`);
 
-        const sizeInMb =
-          Number(response.headers['content-length']) / 1000 / 1000;
-        console.log(`Downloading ${Math.round(sizeInMb * 1000) / 1000}mb...`);
+          const response = await client<Stream>({
+            method: 'GET',
+            url: block.task.status.exportURL || undefined,
+            responseType: 'stream',
+            headers: {
+              Cookie: getTasksRequestCookies.find((cookie) =>
+                cookie.includes('file_token='),
+              ),
+            },
+          });
 
-        const stream = response.data.pipe(createWriteStream(temporaryZipPath));
+          const sizeInMb =
+            Number(response.headers['content-length']) / 1000 / 1000;
+          console.log(`Downloading ${Math.round(sizeInMb * 1000) / 1000}mb...`);
 
-        await new Promise((resolve, reject) => {
-          stream.on('close', resolve);
-          stream.on('error', reject);
-        });
+          const stream = response.data.pipe(
+            createWriteStream(temporaryZipPath),
+          );
 
-        rmSync(backupDirPath, { recursive: true, force: true });
-        mkdirSync(backupDirPath, { recursive: true });
-        await extract(temporaryZipPath, { dir: backupDirPath });
-        unlinkSync(temporaryZipPath);
+          await new Promise((resolve, reject) => {
+            stream.on('close', resolve);
+            stream.on('error', reject);
+          });
 
-        console.log(`✅ Export of ${block.dirName} downloaded and unzipped`);
+          rmSync(backupDirPath, { recursive: true, force: true });
+          mkdirSync(backupDirPath, { recursive: true });
+          await extract(temporaryZipPath, { dir: backupDirPath });
+          unlinkSync(temporaryZipPath);
+
+          console.log(`✅ Export of ${block.dirName} downloaded and unzipped`);
+        }
       }
+
+      // If all blocks are done, break out of the loop
+      if (!enqueuedBlocks.find(({ task }) => task.state !== 'success')) {
+        break;
+      }
+
+      // Reset retries on success
+      retries = 0;
+    } catch (error) {
+      if (!axios.isAxiosError(error) || error.response?.status !== 429) {
+        // Rethrow errors which do not contain an HTTP 429 status
+        // code
+        throw error;
+      }
+
+      console.log(
+        'Received response with HTTP 429 (Too Many Requests), increasing delay...',
+      );
+      retries += 1;
     }
 
-    // If all blocks are done, break out of the loop
-    if (!enqueuedBlocks.find(({ task }) => task.state !== 'success')) {
-      break;
-    }
-
-    // Reset retries on success
-    retries = 0;
-  } catch (error) {
-    if (!axios.isAxiosError(error) || error.response?.status !== 429) {
-      // Rethrow errors which do not contain an HTTP 429 status
-      // code
-      throw error;
-    }
-
-    console.log(
-      'Received response with HTTP 429 (Too Many Requests), increasing delay...',
-    );
-    retries += 1;
+    // Rate limit polling, with incremental backoff
+    await delay(1000 + 1000 * retries);
   }
-
-  // Rate limit polling, with incremental backoff
-  await delay(1000 + 1000 * retries);
 }
 
 console.log('✅ All exports successful');
